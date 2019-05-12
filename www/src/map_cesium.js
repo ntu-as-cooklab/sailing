f8e1b71f3d77859ab3eb28ec371228ed192424bb
En Shih <seanstone5923@gmail.com>
2019-05-12 17:34:11 +0800

    Use cesium map

---
+++ www/src/map_cesium.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function initMap()
+{
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4YjhkY2QwMi02ZTY0LTQ5NmYtOGFmYy01ZjBlM2U2OTMyMjYiLCJpZCI6MTA4NzMsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NTc2NDkyMDR9.0fWoPtwWYd_CAPKUzyQ3TXsgnYamRGBiQTupnIOxYaQ';
+    var viewer = new Cesium.Viewer('map');
+}
