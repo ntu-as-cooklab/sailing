715e460c994a1b1379c1110eb10543cd9011c35a
En Shih <seanstone5923@gmail.com>
2019-05-20 16:15:50 +0800

    Rewrite cfsr_dataset_t

--- cfsr/cfsr.c
+++ cfsr/cfsr.c
@@ -6,15 +6,25 @@
 #include <eccodes.h>
 #include <math.h>
 
-static const char* dataset_str[] = {
-    "ocnu5",
-    "ocnv5",
-};
+typedef struct cfsr_dataset_t
+{
+    codes_nearest* nearest;
+    codes_handle* handle[CFSR_END_YEAR-CFSR_START_YEAR][31][24];
+    char* str;
+    long Ni;
+    long Nj;
+    double lon0;
+    double lat0;
+    double dx;
+    double dy;
+} cfsr_dataset_t;
 
-static codes_nearest* nearest[CFSR_DATASET_MAX];
-static codes_handle* handle[CFSR_DATASET_MAX][CFSR_END_YEAR-CFSR_START_YEAR][31][24];
+cfsr_dataset_t  cfsr_ocnu5 = {.str = "ocnu5"},
+                cfsr_ocnv5 = {.str = "ocnv5"};
+cfsr_dataset_t *CFSR_OCNU5 = &cfsr_ocnu5;
+cfsr_dataset_t *CFSR_OCNV5 = &cfsr_ocnv5;
 
-int cfsr_fetch(cfsr_dataset_t dataset, struct tm date)
+int cfsr_fetch(cfsr_dataset_t* dataset, struct tm date)
 {
     char* filename = cfsr_filename(dataset, date);
     char url[512];
@@ -23,7 +33,7 @@ int cfsr_fetch(cfsr_dataset_t dataset, struct tm date)
     return ftp_getfile(url);
 }
 
-int cfsr_load(cfsr_dataset_t dataset, struct tm date)
+int cfsr_load(cfsr_dataset_t* dataset, struct tm date)
 {
     char* filename = cfsr_filename(dataset, date);
     struct stat st;
@@ -34,6 +44,14 @@ int cfsr_load(cfsr_dataset_t dataset, struct tm date)
     codes_handle* h;
     int err;
     while (h = codes_grib_handle_new_from_file(0, in, &err)) {
+
+        codes_get_long(h,"Ni",&dataset->Ni);
+        codes_get_long(h,"Nj",&dataset->Nj);
+        codes_get_double(h,"latitudeOfFirstGridPointInDegrees",&dataset->lat0);
+        codes_get_double(h,"longitudeOfFirstGridPointInDegrees",&dataset->lon0);
+        codes_get_double(h,"jDirectionIncrementInDegrees",&dataset->dy);
+        codes_get_double(h,"iDirectionIncrementInDegrees",&dataset->dx);
+
         long dataDate, dataTime, startStep;
         codes_get_long(h, "dataDate", &dataDate);
         codes_get_long(h, "dataTime", &dataTime);
@@ -41,25 +59,25 @@ int cfsr_load(cfsr_dataset_t dataset, struct tm date)
         date.tm_mday = dataDate%100;
         date.tm_hour = dataTime/100 + startStep;
         date.tm_min = dataTime%100;
-        handle[dataset][date.tm_year-CFSR_START_YEAR][date.tm_mday-1][date.tm_hour] = h;
+        dataset->handle[date.tm_year-CFSR_START_YEAR][date.tm_mday-1][date.tm_hour] = h;
     }
     fclose(in);
     return 0;
 }
 
-char* cfsr_filename(cfsr_dataset_t dataset, struct tm date)
+char* cfsr_filename(cfsr_dataset_t* dataset, struct tm date)
 {
     static char filename[128];
-    snprintf(filename, sizeof(filename), "%s.gdas.%04u%02u.grb2", dataset_str[dataset], date.tm_year, date.tm_mon+1);
+    snprintf(filename, sizeof(filename), "%s.gdas.%04u%02u.grb2", dataset->str, date.tm_year, date.tm_mon+1);
     return filename;
 }
 
-static codes_handle* cfsr_handle(cfsr_dataset_t dataset, struct tm date)
+static codes_handle* cfsr_handle(cfsr_dataset_t* dataset, struct tm date)
 {
-    return handle[dataset][date.tm_year-CFSR_START_YEAR][date.tm_mday-1][date.tm_hour];
+    return dataset->handle[date.tm_year-CFSR_START_YEAR][date.tm_mday-1][date.tm_hour];
 }
 
-double cfsr_value(cfsr_dataset_t dataset, struct tm date, double lat, double lon)
+double cfsr_value(cfsr_dataset_t* dataset, struct tm date, double lat, double lon)
 {
     codes_handle* h = cfsr_handle(dataset, date);
     if (!h) {
@@ -68,14 +86,14 @@ double cfsr_value(cfsr_dataset_t dataset, struct tm date, double lat, double lon
     }
     
     int err;
-    if(!nearest[dataset]) nearest[dataset] = codes_grib_nearest_new(h, &err);
+    if(!dataset->nearest) dataset->nearest = codes_grib_nearest_new(h, &err);
     static double lats[4]={0,};
     static double lons[4]={0,};
     static double values[4]={0,};
     static double distances[4]={0,};
     static int indexes[4]={0,};
     static size_t size=4;
-    codes_grib_nearest_find(nearest[dataset], h, lat, lon, CODES_NEAREST_SAME_GRID, 
+    codes_grib_nearest_find(dataset->nearest, h, lat, lon, CODES_NEAREST_SAME_GRID, 
                             lats, lons, values, distances, indexes, &size);
     return cfsr_idw(values, distances, size);
 }
@@ -89,16 +107,16 @@ double cfsr_idw(double* values, double* distances, size_t size)
     return result;
 }
 
-void __attribute__((destructor)) cfsr_free()
+void cfsr_free(cfsr_dataset_t* dataset)
 {
-    printf("Freeing CFSR resources\n");
+    codes_grib_nearest_delete(dataset->nearest);
 
-    for (int i = 0; i < CFSR_DATASET_MAX; i++)
-        codes_grib_nearest_delete(nearest[i]);
-    
-    for (int i = 0; i < CFSR_DATASET_MAX; i++)
-        for (int j = 0; j < CFSR_END_YEAR-CFSR_START_YEAR; j++)
-            for (int k = 0; k < 31; k++)
-                for (int l = 0; l < 24; l++)
-                    codes_handle_delete(handle[i][j][k][l]);
+    for (int j = 0; j < CFSR_END_YEAR-CFSR_START_YEAR; j++)
+        for (int k = 0; k < 31; k++)
+            for (int l = 0; l < 24; l++)
+                codes_handle_delete(dataset->handle[j][k][l]);
+}
+
+int cfsr_bilinear(cfsr_dataset_t* dataset)
+{
 }
--- cfsr/cfsr.h
+++ cfsr/cfsr.h
@@ -7,22 +7,19 @@ extern  "C" {
 
 #include <time.h>
 
-typedef enum cfsr_dataset_t
-{
-    CFSR_OCNU5,
-    CFSR_OCNV5,
-    CFSR_DATASET_MAX,
-} cfsr_dataset_t;
+typedef struct cfsr_dataset_t cfsr_dataset_t;
 
-int cfsr_fetch(cfsr_dataset_t dataset, struct tm date);
-int cfsr_load(cfsr_dataset_t dataset, struct tm date);
-char* cfsr_filename(cfsr_dataset_t dataset, struct tm date);
-double cfsr_value(cfsr_dataset_t dataset, struct tm date, double lat, double lon);
+int cfsr_fetch(cfsr_dataset_t* dataset, struct tm date);
+int cfsr_load(cfsr_dataset_t* dataset, struct tm date);
+char* cfsr_filename(cfsr_dataset_t* dataset, struct tm date);
+double cfsr_value(cfsr_dataset_t* dataset, struct tm date, double lat, double lon);
 double cfsr_idw(double* values, double* distances, size_t size);
-void cfsr_free();
+void cfsr_free(cfsr_dataset_t* dataset);
 
 #define CFSR_START_YEAR 1979
 #define CFSR_END_YEAR 2012
+
+extern cfsr_dataset_t *CFSR_OCNU5, *CFSR_OCNV5;
 
 #ifdef  __cplusplus
 }
