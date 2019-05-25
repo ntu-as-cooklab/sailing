88b898fdf89bfd85c90949bbeb6fc9337eaf3ea4
En Shih <seanstone5923@gmail.com>
2019-05-25 14:57:26 +0800

    Cesium: use MapBox provider as default

--- www/src/map_cesium.js
+++ www/src/map_cesium.js
@@ -2,6 +2,13 @@
 
 function initMap()
 {
+    var imageryProviders = Cesium.createDefaultImageryProviderViewModels()
+    Cesium.MapboxApi.defaultAccessToken = "pk.eyJ1Ijoic2VhbnN0b25lNTkyMyIsImEiOiJjaW1kOTJrZGYwMDUxdHNra2wwOGN0cTY1In0.n0m5Euk9SWbiNt4RZi8vkQ";
     Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4YjhkY2QwMi02ZTY0LTQ5NmYtOGFmYy01ZjBlM2U2OTMyMjYiLCJpZCI6MTA4NzMsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NTc2NDkyMDR9.0fWoPtwWYd_CAPKUzyQ3TXsgnYamRGBiQTupnIOxYaQ';
-    var viewer = new Cesium.Viewer('map');
+    var viewer = new Cesium.Viewer('map', {
+        requestRenderMode : true,
+        sceneMode: Cesium.SceneMode.SCENE2D,
+        imageryProviderViewModels: imageryProviders,
+        selectedImageryProviderViewModel: imageryProviders[4],
+    });
 }
