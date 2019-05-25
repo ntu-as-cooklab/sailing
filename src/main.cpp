620c43f4e4557b7bd57f36a0b37703c5488cd011
En Shih <seanstone5923@gmail.com>
2019-05-25 10:28:43 +0800

    src: rmove main.cpp

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "vec2.h"
-
-int main(void)
-{
-	printf(	"*************** Sailing Simulation ***************\n"
-			"Build: %s %s\n", __DATE__, __TIME__);
-   	return 0;
-}
