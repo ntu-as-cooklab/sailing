513693b588ef118fe10df6f294b9246ca8b09631
En Shih <seanstone5923@gmail.com>
2019-05-25 10:22:45 +0800

    apps: fix

---
+++ apps/sailing.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "vec2.h"
+
+int main(void)
+{
+	printf(	"*************** Sailing Simulation ***************\n"
+			"Build: %s %s\n", __DATE__, __TIME__);
+   	return 0;
+}
---
+++ apps/test_nc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "cfsr_nc.h"
+
+int main (void)
+{
+    struct tm date;
+    date.tm_year = 1979;
+    date.tm_mon = 0;
+    date.tm_mday = 1;
+
+    for (int i = 0; i < 48; i++) {
+        double ou = cfsr_nc_bilinear(CFSR_NC_OCNU5, date, (latlon_t){22.495, 129.989});
+        printf("val: %f\n", ou);
+        date.tm_hour++;
+        mktime(&date);
+    }
+
+    return 0;
+}
