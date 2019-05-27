dcd1be83ef5c318baa22aef9e71cb0429febf083
En Shih <seanstone5923@gmail.com>
2019-05-27 10:00:18 +0800

    Client code cleanup

--- www/src/main.js
+++ www/src/main.js
@@ -1,33 +1,11 @@
 "use strict";
 
-var container, controls, info, outputlist, doConnect, icolor, iyear, heading, project, dataset_avg, dataset_1979, dataset_1982;
-var startdate, enddate, days;
 var wsClient;
-var voyage = [];
 
 $(function(){main();});
 
 function main()
 {
-	// Get DOM elements
-	container 	= document.getElementById("container");
-	controls 	= document.getElementById("controls");
-	info 		= document.getElementById("info");
-	startdate 	= document.getElementById("startdate");
-	enddate 	= document.getElementById("enddate");
-	days 		= document.getElementById("days");
-	outputlist 	= document.getElementById("outputlist");
-	doConnect 	= document.getElementById("doConnect");
-	icolor 	= document.getElementById("icolor");
-	iyear 	= document.getElementById("iyear");
-	heading 	= document.getElementById("heading");
-	project 	= document.getElementById("project");
-
-	dataset_avg 	= document.getElementById("dataset_avg");
-	dataset_1979 	= document.getElementById("dataset_1979");
-	dataset_1982 	= document.getElementById("dataset_1982");
-
-	//alert("Init");
 
 	wsClient = new WsClient();
 	wsClient.connect();
@@ -35,76 +13,9 @@ function main()
 	initMap();
 }
 
-function setParam(e)
-{
-	if(e.name) wsClient.send(e.name + "= " + e.value);
-
-	if(e == heading)
-	{
-		if(heading.value<0) heading.value = Number(heading.value) + 360;
-		heading.value = heading.value%360;
-		wsClient.send("dir= " + Math.sin(heading.value*Math.PI/180.0) + " " + Math.cos(heading.value*Math.PI/180.0));
-	}
-
-	var year;
-	/*if (dataset_avg.checked)
-	{
-		wsClient.send("dataset= 2"); year = 9999;
-	}
-	if (dataset_1979.checked)
-	{
-		wsClient.send("dataset= 2"); year = 1979;
-	}
-	if (dataset_1982.checked)
-	{
-		wsClient.send("dataset= 2"); year = 1982;
-	}*/
-
-	if(e == iyear)
-	{
-		year = e.value;
-		startdate.min = year + "-01-01";
-		startdate.value = year + "-01-01";
-		startdate.max = (Number(year)+1) + "-12-31";
-		setDays();
-		enddate.min = startdate.value;
-		enddate.max = (Number(year)+1) + "-12-31";
-		wsClient.send(startdate.name + "= " + startdate.value);
-	}
-}
-function setStartDate() { setEndDate(); enddate.min = startdate.value; if (new Date(enddate.value).getTime() < new Date(startdate.value).getTime()) enddate.value = startdate.value; setParam(enddate); }
-function setEndDate() { days.value = Math.ceil((new Date(enddate.value).getTime() - new Date(startdate.value).getTime())/86400000); setParam(days); }
-function setDays() { var date = new Date(startdate.value);
-						date.setTime(date.getTime() + days.value * 86400000);
-						enddate.value = date.toISOString().substr(0,10); setParam(enddate); }
-
 function connect()
 {
 	wsClient.connect();
-}
-
-function resetParams()
-{
-
-}
-
-function newRecord()
-{
-	var projectName = prompt("請輸入專案名稱", "New");
-
-    if (projectName != null)
-	{
-        project.innerHTML = projectName;
-		wsClient.send("project= " + projectName);
-    }
-
-	for (var n=0; n<voyage.length; n++)
-	{
-		map.removeLayer(voyage[n].layerGroup);
-		voyage[n].deleted = true;
-		var item = document.getElementById("outputitem"+n);
-		if(item) item.parentNode.removeChild(item);
-	}
 }
 
 function setcolor(e, n)
@@ -131,28 +42,4 @@ function del(n)
 	var item = document.getElementById("outputitem"+n);
 	if(item) item.parentNode.removeChild(item);
 	wsClient.send("delete " + voyage[n].name);
-}
-
-function clearRecord()
-{
-	for (var n=0; n<voyage.length; n++)
-		del(n);
-}
-
-function toggleMode(i)
-{
-	if (i == 0)
-	{
-		document.getElementById("ship").disabled = true;
-		document.getElementById("altitude").disabled = true;
-		document.getElementById("windlimit").disabled = true;
-		document.getElementById("sailopenhours").disabled = true;
-	}
-	else
-	{
-		document.getElementById("ship").disabled = false;
-		document.getElementById("altitude").disabled = false;
-		document.getElementById("windlimit").disabled = false;
-		document.getElementById("sailopenhours").disabled = false;
-	}
 }
--- www/src/map.js
+++ www/src/map.js
@@ -2,8 +2,6 @@
 
 var map;
 var orig, dest;
-var datasetName = ["1979~2009 平均資料", "1979~2009 連續資料", "單年資料"];
-var modeName = ["瓶中信 (無風帆)", "隨風漂流 (航向=風向)", "固定目的地", ""];
 
 var mainlatlonlines;
 var latlonlines;
@@ -96,16 +94,7 @@ function initMap()
 			}),
 		draggable: true, continousWorld : true }).addTo(map);
 
-	// var destIcon = L.icon({
-	// 		iconUrl: 	'dest.png',
-	// 		iconSize:     [iconsize, iconsize], // size of the icon
-	// 		iconAnchor:   [iconsize/2, iconsize/2], // point of the icon which will correspond to marker's location
-	// 		popupAnchor:  [0, iconsize/2] // point from which the popup should open relative to the iconAnchor
-	// });
-
-	//function setDest(latlng) { dest ? dest.setLatLng(latlng) : dest = L.marker(latlng, { icon: destIcon, draggable: true, continousWorld : true }).addTo(map); }
 	setOrig(curr_date, new L.LatLng(-10.0, 160.0));
-	//setDest(new L.LatLng(-14.0, 188.5));
 
 	L.easyButton('fa-globe', function(btn, map){
 		var antarctica = [-77,70];
@@ -115,16 +104,14 @@ function initMap()
 	/** Events **/
 
 	function onMapClick(e) { setOrig(curr_date, e.latlng); }
-	function onMapMouseMove(e) { info.innerHTML = e.latlng.lat.toFixed(5) + ', ' + e.latlng.lng.toFixed(5); }
+	function onMapMouseMove(e) { document.getElementById("info").innerHTML = e.latlng.lat.toFixed(5) + ', ' + e.latlng.lng.toFixed(5); }
 	//function onOrigDragEnd(e) { wsClient.send("orig = " + orig.getLatLng().lat + "," + orig.getLatLng().lng); }
-	//function onDestDragEnd(e) { wsClient.send("dest = " + dest.getLatLng().lat + "," + dest.getLatLng().lng); }
 	map.on('click', onMapClick);
 	map.on('contextmenu', onMapClick);
 	map.on('mousemove', onMapMouseMove);
-	orig.on('dragend', onOrigDragEnd);
-	//dest.on('dragend', onDestDragEnd);
+	//orig.on('dragend', onOrigDragEnd);
 
-	toggleLatLonLines();
+	//toggleLatLonLines();
 }
 
 function toggleLatLonLines()
