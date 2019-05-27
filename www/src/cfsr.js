6b1355b44821855403ee2bb0380f1073a2012053
En Shih <seanstone5923@gmail.com>
2019-05-27 11:46:08 +0800

    Organize ccode

--- www/src/cfsr.js
+++ www/src/cfsr.js
@@ -1,4 +1,4 @@
 "use strict";
 
-var min_date = new Date(1979, 0,  1, 0);
-var max_date = new Date(2011, 11, 31, 0);
+const DATE_MIN = new Date(1979, 0,  1, 0);
+const DATE_MAX = new Date(2011, 11, 31, 0);
--- www/src/session.js
+++ www/src/session.js
@@ -1,8 +1,10 @@
 "use strict";
 
-var session = {
+var Session = {
     user: 0,
     token: "",
+    startdate: DATE_MIN,
+    enddate: DATE_MIN,
     paths: {
     }
 };
@@ -12,8 +14,10 @@ function request_path()
 	var startdate = new Date(document.getElementById("startdate").value);
 	var startloc = orig.getLatLng();
 	ws.send(CBOR.encode({
-		cmd: "newPath",
-		date: [startdate.getYear(),startdate.getMonth(),startdate.getDay(),startdate.getHours()],
-		orig: [startloc.lat, startloc.lng],
+        cmd:    "newPath",
+        user:   Session.user,
+        token:  Session.token,
+		date:   [startdate.getYear(),startdate.getMonth(),startdate.getDay(),startdate.getHours()],
+        orig:   [startloc.lat, startloc.lng],
 	}));
 }
