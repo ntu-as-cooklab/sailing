522b5bcdb49c3d632e0a063362add4ca2cd3892e
En Shih <seanstone5923@gmail.com>
2019-05-30 12:01:54 +0800

    Add sailing parameters

--- src/path.hpp
+++ src/path.hpp
@@ -7,11 +7,16 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "model.hpp"
+#include "vec2.h"
 
 typedef struct pathpt_t
 {
     struct tm date;
     latlon_t loc;
+    bool sailopen;
+    vec2 ocean;
+    vec2 wind;
+    vec2 wind_gain;
 } pathpt_t;
 
 typedef struct path_t
@@ -25,6 +30,12 @@ typedef struct path_t
     DATASET dataset;
     MODE mode;
     std::vector<pathpt_t> pts;
+
+    /* Parameters */
+	float 	altitude 		= 2;
+	float 	windlimit 		= 8;
+	int 	sailopenhours 	= 12;
+	float 	alpha 			= 0.11;				// parameter for wind profile power law
 } path_t;
 
 #endif
