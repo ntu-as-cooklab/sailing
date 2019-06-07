68f7914d14e57144fb99b5451c9d8921e776db47
En Shih <seanstone5923@gmail.com>
2019-06-07 09:42:04 +0800

    Adjust max date

--- www/src/session.js
+++ www/src/session.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const DATE_MIN = new Date(1979, 0,  1, 0);
-const DATE_MAX = new Date(2011, 11, 31, 0);
+const DATE_MAX = new Date(2011, 3, 31, 0);
 
 var Session = {
     runId: 0,
